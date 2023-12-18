991357b868be45b1f55a3442aef5e3de0ad59488
James Jones <atari@theinnocuous.com>
2023-12-17 16:56:27 -0800

    Add local env setup and cleanups
    -env.bat: Set up the environment variables based
     on where the SDK is checked out.
    
    -Add .gitignore files to ignore build artifacts
    
    -Ensure all makefiles properly clean their
     artifacts.
    
    -Move binaries to win32 subdirectory to make space
     for future Linux support.

---
+++ env.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM Find the SDK absolute path
+set NUONSDK=%~d0%~p0
+REM Remove trailing backslash
+set NUONSDK=%NUONSDK:~0,-1%
+
+REM Add the win32 binaries to the search path
+set Path=%Path%;%NUONSDK%\vmlabs\bin\win32
+
+REM Set the VMLABS variable to point at the VMLABS SDK Root
+set VMLABS=%NUONSDK%\vmlabs
